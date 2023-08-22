272dfd300001be87cd4f2e0da040e598bf75abcf
Jean Dias <devjeandias@gmail.com>
2023-08-22 03:33:42 -0300

    cria componente Header variaveis de cores e ajustes no bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>Harry Potter API</h1>
+      <main>
+        <Header />
+      </main>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.scss';
+function Header() {
+    return (
+        <header class="header" id="header">
+            <h1 class="header__title">Harry Potter</h1>
+            <h2 class="header__subtitle">Personagens</h2>
+            <p>Adentre o mundo de magia e mistério de Harry Potter! Conheça Harry, Hermione, Rony e outros personagens icônicos que povoam as páginas desta saga encantadora. Embarque em uma jornada épica repleta de amizade, coragem e feitiços inesquecíveis. Descubra a magia que cativou corações em todo o mundo.</p>
+        </header>
+    );
+}
+
+export default Header;
